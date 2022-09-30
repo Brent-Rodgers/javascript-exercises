f80efe9b80f8899c9ce9164ca2b5654c15f274bd
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-09-29 20:41:52 -0600

    solve exercise 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let start = Math.min(x, y);
+    let end = Math.max(x, y);
 
+    if (x < 0 | 
+        y < 0 | 
+        !Number.isInteger(x) | 
+        !Number.isInteger(y)) {
+        return("ERROR");
+    }
+
+    let result = 0;
+    for (i = start; i <= end; i++) {
+        result += i;
+    }
+    return(result);
 };
 
 // Do not edit below this line
