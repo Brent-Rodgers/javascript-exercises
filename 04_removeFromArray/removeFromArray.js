1aa595ec6cfd145e4cef97cf6380fa9a70916bf9
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-09-29 20:31:48 -0600

    solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (valueToRemove of args){
+        let indexToRemove = array.indexOf(valueToRemove);
+        if (indexToRemove != -1) {
+            array.splice(indexToRemove, 1);
+        }
+    }
 
+    return(array);
 };
 
 // Do not edit below this line
