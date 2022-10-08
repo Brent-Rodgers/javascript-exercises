21c9a9cfdd1f9595d831fa11e26df98d3b0e942c
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-10-08 15:54:38 -0600

    solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => total + number, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, number) => total * number, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	result = 1;
+  for (let i = 2; i <= num; i++) {
+    result = result * i
+  }
+  return result;
 };
 
 // Do not edit below this line
