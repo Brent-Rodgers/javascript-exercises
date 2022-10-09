6138d993f7193b15814e0c75a635dfc1dfcb8fde
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-10-09 06:59:18 -0600

    solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanedString = string.toLowerCase().replace(/[^0-9a-z]/gi, '')
+    
+    let reversed = cleanedString.split("").reverse().join("")
 
+    return cleanedString === reversed;
 };
 
 // Do not edit below this line
