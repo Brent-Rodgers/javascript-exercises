e5e608c8778a43054fd76b4815d64da4d5618ec2
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-10-09 07:24:31 -0600

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let previous = 0;
+    let current = 1;
+    let next;
 
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+
+    for (let i = 1; i < num; i++){
+        next = current + previous;
+        previous = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
